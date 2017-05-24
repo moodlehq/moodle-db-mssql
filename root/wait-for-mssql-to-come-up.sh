42b779f4c17b5e4d8eb34f50ba678152b1e81b27
Dan Poltawski <dan@moodle.com>
2017-05-24 22:04:34 +0100

    Add a timeout for the mssql wait

--- root/wait-for-mssql-to-come-up.sh
+++ root/wait-for-mssql-to-come-up.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+i=0
 # Wait for the SQL Server to come up.
 until nc -z -w2 127.0.0.1 1433
 do
     echo "[moodle-db-mssql] Waiting 5s for mssql to come up setup"
     sleep 5
+    i=$((i+1))
+    if [ $i -gt 50 ]; then
+        echo "[moodle-db-mssql] timed out waiting for server to come up after 4 mins"
+        exit 0;
+    fi
 done
 echo "[moodle-db-mssql] SQL Server UP"
